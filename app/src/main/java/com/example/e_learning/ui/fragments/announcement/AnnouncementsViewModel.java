fa59e32715d0625bf7cdaa7e986eab5b925f5e73
Moataz Mohamed <moataz.mohamed@kashat.com.eg>
2020-07-04 01:58:10 +0200

    adding Chunk (interceptor for network requests) adding okhttpclient adding manifest flag to call http urls
    Signed-off-by: Moataz Mohamed <moataz.mohamed@kashat.com.eg>

--- app/src/main/java/com/example/e_learning/ui/fragments/announcement/AnnouncementsViewModel.java
+++ app/src/main/java/com/example/e_learning/ui/fragments/announcement/AnnouncementsViewModel.java
@@ -13,10 +13,10 @@ import retrofit2.Response;
 public class AnnouncementsViewModel extends ViewModel {
 
     MutableLiveData<AnnouncementsModel> announcementsModelMutableLiveData = new MutableLiveData<>();
-    private MutableLiveData<Integer> studentId ;
-     MutableLiveData<String> error = new MutableLiveData<>();
-    MutableLiveData<Integer> getStudentId() {
+    MutableLiveData<String> error = new MutableLiveData<>();
+    private MutableLiveData<Integer> studentId;
 
+    MutableLiveData<Integer> getStudentId() {
         if (studentId == null) {
             studentId = new MutableLiveData<>();
         }
@@ -25,16 +25,13 @@ public class AnnouncementsViewModel extends ViewModel {
     }
 
     MutableLiveData<AnnouncementsModel> getResponse() {
-
         if (announcementsModelMutableLiveData == null) {
             announcementsModelMutableLiveData = new MutableLiveData<>();
         }
         return announcementsModelMutableLiveData;
-
     }
 
-    void getAnnouncements()
-    {
+    void getAnnouncements() {
         StudentClient.getINSTANCE().getAnnouncements(studentId.getValue()).enqueue(new Callback<AnnouncementsModel>() {
             @Override
             public void onResponse(Call<AnnouncementsModel> call, Response<AnnouncementsModel> response) {
